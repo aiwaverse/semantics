772e5a445643ce9fbd1e8543aece83a2d36f929d
aiwavision <aiwavision@protonmail.com>
2022-11-27 19:02:32 -0300

    Implementada l0

--- lib/l_zero.ml
+++ lib/l_zero.ml
@@ -1,5 +1,3 @@
-open Either
-open List
 open Option
 
 type expr =
@@ -28,65 +26,60 @@ let rec is_numerical e =
 
 let rec step e =
   match e with
-  | True -> Left "Can't step True"
-  | False -> Left "Can't step False"
-  | Zero -> Left "Can't step Zero"
+  | True -> None
+  | False -> None
+  | Zero -> None
   | Succ _ -> apply_succ e
   | Pred _ -> apply_pred e
   | IsZero _ -> apply_is_zero e
-  | If (_, _, _) -> apply_if e
-
-and apply_if e =
-  match e with
-  | If (True, e1, _) -> Right e1
-  | If (False, _, e2) -> Right e2
-  | If (cond, e1, e2) -> (
-      match step cond with
-      | Left err -> Left ("Can't apply If, internal step failed with: " ^ err)
-      | Right ee -> Right (If (ee, e1, e2)))
-  | _ -> Left ("Can't apply If on " ^ print_expr e)
-
-and apply_is_zero e =
-  match e with
-  | IsZero Zero -> Right True
-  | IsZero (Succ _) -> Right False
-  | IsZero ee -> (
-      match step ee with
-      | Left err ->
-          Left ("Can't apply IsZero, internal step failed with: " ^ err)
-      | Right eee -> Right (IsZero eee))
-  | _ -> Left ("Can't apply IsZero on " ^ print_expr e)
-
-and apply_pred e =
-  match e with
-  | Pred Zero -> Right Zero
-  | Pred (Succ ee) ->
-      if is_numerical ee then Right ee
-      else Left ("Can't apply Pred, value is not numerical: " ^ print_expr ee)
-  | Pred ee -> (
-      match step ee with
-      | Left err -> Left ("Can't apply Pred, internal step failed with: " ^ err)
-      | Right eee -> Right (Pred eee))
-  | _ -> Left ("Can't apply Pred on" ^ print_expr e)
+  | If _ -> apply_if e
 
 and apply_succ e =
   match e with
   | Succ ee -> (
-      match step ee with
-      | Left err -> Left ("Can't apply Succ, internal step failed with: " ^ err)
-      | Right eee -> Right (Succ eee))
-  | _ -> Left ("Can't apply Pred on" ^ print_expr e)
+      match step ee with None -> None | Some stepped -> Some (Succ stepped))
+  | _ -> None
+
+and apply_pred e =
+  match e with
+  | Pred nv -> (
+      if is_numerical nv then
+        match nv with Zero -> Some Zero | Succ e -> Some e | _ -> None
+      else
+        match step nv with None -> None | Some stepped -> Some (Pred stepped))
+  | _ -> None
+
+and apply_is_zero e =
+  match e with
+  | IsZero nv -> (
+      if is_numerical nv then
+        match nv with Zero -> Some True | Succ _ -> Some False | _ -> None
+      else
+        match step nv with
+        | None -> None
+        | Some stepped -> Some (IsZero stepped))
+  | _ -> None
+
+and apply_if e =
+  match e with
+  | If (True, opt_true, _) -> Some opt_true
+  | If (False, _, opt_false) -> Some opt_false
+  | If (cond, opt_true, opt_false) -> (
+      match step cond with
+      | None -> None
+      | Some stepped -> Some (If (stepped, opt_true, opt_false)))
+  | _ -> None
 
 let rec eval e =
   match step e with
-  | Left stop -> [ print_expr e; "Evaluation stopped at " ^ stop ]
-  | Right ee -> print_expr e :: eval ee
+  | None -> [ print_expr e ]
+  | Some stepped -> print_expr e :: eval stepped
 
 let result e =
-  let rec second_to_last lst =
+  let rec last lst =
     match lst with
     | [] -> None
-    | [ res; _ ] -> Some res
-    | _ :: xxs -> second_to_last xxs
+    | [ result ] -> Some result
+    | _ :: xxs -> last xxs
   in
-  second_to_last (eval e)
+  Option.value (last (eval e)) ~default:""
